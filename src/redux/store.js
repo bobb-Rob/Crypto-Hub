be56c19384bf778ad9e2fdb242c48ebd7aee3ef0
Robertson Arthur <robertsonakpan@gmail.com>
2022-07-28 15:46:03 +0100

    add test for cryptoContainer and cryptoReducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import cryptoReducers from './cryptoReducers';
 
-console.log(cryptoReducers);
 const store = configureStore({
   reducer: {
     crypto: cryptoReducers,
