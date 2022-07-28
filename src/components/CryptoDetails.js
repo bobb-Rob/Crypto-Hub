fd3296d85734923c5a6828f6d7a680e8bc7c4e2b
Robertson Arthur <robertsonakpan@gmail.com>
2022-07-28 09:30:08 +0100

    added details page and it contnes

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux/es/exports';
+import Icon from 'react-icons-kit';
+import { arrowCircleLeft } from 'react-icons-kit/fa/arrowCircleLeft';
 
-const CryptoDetails = () => (
-  <div>CryptoDetails</div>
-);
+const CryptoDetails = () => {
+  const state = useSelector((state) => state.crypto);
+  const params = useParams();
+  const currentCrypto = state.find((item) => item.id === params.cryptoId);
 
+  const {
+    id,
+    icon,
+    name,
+    symbol,
+    rank,
+    price,
+    priceBtc,
+    volume,
+    marketCap,
+    availableSupply,
+    totalSupply,
+    priceChange1h,
+    priceChange1d,
+    priceChange1w,
+    websiteUrl,
+    twitterUrl,
+  } = currentCrypto;
+
+  return (
+    <div className="crypto-details-page">
+      <Link to="/">
+          <i className="go-back">
+            <Icon icon={arrowCircleLeft} size={25} />
+          </i>
+        </Link>
+    </div>
+  );
+};
 export default CryptoDetails;
